3e69f1e300a35e1735c99256be953ef0ad36f4f0
Gabriel Mendoza <angrygabo@gmail.com>
2020-04-29 16:00:55 +0200

    Se añaden efectos en contenedores

---
+++ js/boogo.js
@@ -0,0 +1,23 @@
+function viewportTrigger()  {  
+    $('.viewport--trigger').each(function(reveal) {
+        $(this).delay(80*reveal).animate({ opacity: 1 },function(){
+            if (onview($(this))) {
+                $(this).addClass('in-viewport');
+            }
+        });
+    });
+    function onview(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height()/2;
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    }  
+}  
+
+$(document).ready(function() {
+    viewportTrigger();
+    $(window).on('scroll', function () { 
+        viewportTrigger();
+    });  
+});
