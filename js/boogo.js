c6e923da82b038bc611c402edc1be965938a2e67
Gabriel Mendoza <angrygabo@gmail.com>
2020-04-30 18:51:47 +0200

    ajuste pagina prueba

--- js/boogo.js
+++ js/boogo.js
@@ -1,25 +1,34 @@
 // reveal items
 function viewportTrigger() {
-    $('.viewport--trigger').each(function (reveal) {
-        $(this).delay(100 * reveal).animate({ opacity: 1 }, function () {
+    $('.viewport').each(function (i) {
             if (onview($(this))) {
-                $(this).addClass('in-viewport');
+                $(this).delay(150 * i).animate({ opacity: 1 }, function () {
+                    $(this).addClass('in-viewport');
+                    setTimeout(function() {
+                        $('.in-viewport').removeClass('viewport');
+                    }, 200);
+                });
             }
-        });
     });
     function onview(elem) {
         var docViewTop = $(window).scrollTop();
         var docViewBottom = docViewTop + $(window).height();
         var elemTop = $(elem).offset().top;
-        var elemBottom = elemTop + $(elem).height() - 100;
+        var elemBottom = elemTop + $(elem).height()-300;
         return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
     }
+}
+
+function parallaxScroll() {
+    var scrolled = $(window).scrollTop();
+    $('.parallax-content').css('top', (0 - (scrolled * 0.8)) + 'px');
 }
 
 $(document).ready(function () {
     viewportTrigger();
     $(window).on('scroll', function () {
         viewportTrigger();
+        parallaxScroll();
     });
 });
 
